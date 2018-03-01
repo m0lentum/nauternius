b3354a6c069c0ede7f7f760d0d472a6f027414c6
Juha Tapio Koivistoinen <juha.t.koivistoinen@jyu.fi>
2018-03-01 15:00:18 +0200

    Pallosysteemin alku lisätty

---
+++ Assets/Scripts/BallSocket.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Täysin keskeneräinen scripti ja error handling tässä on Jeesuksen käsissä.
+public class BallSocket : MonoBehaviour {
+
+    public float pullForce;
+    public float stillSpeed;
+    
+    public GameObject targetObj;
+
+    private bool isOn;
+    public bool IsOn
+    {
+        get { return isOn; }
+        set
+        {
+            if (isOn != value)
+            {
+                Debug.Log("BallSocketista vaihdettu bool: " + isOn);
+                isOn = value;
+                TriggerTargetObject();
+            }
+        }
+    }
+
+    void Start ()
+    {
+        //targetObj = editorissa asetettu
+        stillSpeed = 0.2f;
+        pullForce = 15f;
+	}
+
+	void Update ()
+    {
+
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("BallSocket trigger: " + other.name);
+
+        GameObject otherObj = other.gameObject;
+        if (otherObj.tag == "ball")
+        {
+            otherObj.transform.position = Vector3.MoveTowards(otherObj.transform.position, transform.position, pullForce * Time.deltaTime);
+        }
+
+        //GetComponent pois jatkuvasti toistettavasta funktiosta, vois esim. hallinnoida ontriggerenterissä/exitissä?
+        if (otherObj.GetComponent<Rigidbody>().velocity.magnitude < stillSpeed) IsOn = true;
+        
+    }
+
+    void TriggerTargetObject()
+    {
+        Debug.Log("BallSocketista kutsuttu TriggerTargetObject");
+        targetObj.GetComponent<BallTriggerTest>().BallTrigger();
+    }
+
+}
---
+++ Assets/Scripts/BallTriggerTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTriggerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BallTrigger()
+    {
+        Debug.Log("BallTriggerTestistä kutsuttu BallTrigger");
+        transform.position = transform.position - new Vector3(transform.position.x, transform.position.y - 5, transform.position.z);
+    }
+}
