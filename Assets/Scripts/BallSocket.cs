d9637c6c16d59000beb6da30473e6d132c0a19dc
Saska Sinkkonen <saansink@student.jyu.fi>
2018-03-19 15:28:16 +0200

    Pallojen toiminta suunnitelman mukaiseksi

--- Assets/Scripts/BallSocket.cs
+++ Assets/Scripts/BallSocket.cs
@@ -11,6 +11,8 @@ using UnityEngine;
 public class BallSocket : MonoBehaviour {
     
     [SerializeField] private GameObject targetObj;
+    [SerializeField] private GameObject targetBall;
+
     [SerializeField] private float maxPullForce;
     [SerializeField] private float minPullForce;
     [SerializeField] private float maxSocketTimer;
@@ -18,42 +20,52 @@ public class BallSocket : MonoBehaviour {
     [SerializeField] private float forceFieldPower;
     [SerializeField] private Material activatedMaterial;
 
+    //publicit pois testin jälkeen
     private float curPullForce;
-    private float socketTimer;
-    private bool isForceFieldActive;
-    private Rigidbody ballRb;
-    private Rigidbody collRb;
+    public float socketTimer;
+    public bool isForceFieldActive;
+    public GameObject ball;
+    public Rigidbody ballRb;
+    public Rigidbody collRb;
 
     //Ottaa lähelle tulevan objektin rigidbodyn talteen
-    //Bugaa helposti, jos forcefield päällä ja 2 objektia tulee sisään samaan aikaan, koska ottaa uuden RBn talteen ennen kuin vanha on käsitelty. Onko paha?
+    //Bugaa helposti, jos forcefield päällä ja 2 objektia tulee sisään samaan aikaan(?), koska ottaa uuden RBn talteen ennen kuin vanha on käsitelty. Onko paha?
     private void OnTriggerEnter(Collider other)
     {
+        //Ottaa talteen pallon ja sen RigidBodyn
         if (other.tag == "ball" && !isForceFieldActive)
         {
             curPullForce = maxPullForce;
+            ball = other.gameObject;
             ballRb = other.GetComponent<Rigidbody>();
         }
-        else collRb = other.transform.root.GetComponent<Rigidbody>();
+        //Ottaa talteen törmäävän objektin RigidBodyn, kun ForceField on päällä
+        //todo ei toimi, jos rigidbody ei ole hierarkian ylimmässä objektissa
+        else
+        {
+            Debug.Log(other.transform.root.name);
+            collRb = other.transform.root.GetComponent<Rigidbody>();
+        }
     }
 
     void OnTriggerStay(Collider other)
     {
-        //Työntää pelaajan pois jos force field on päällä eli socket on aktiivinen
-        if (isForceFieldActive && other.tag != "ball")
+        //Imee palloja keskelle, jos ForceField ei ole päällä
+        if (other.gameObject == ball)
+        {
+            curPullForce -= 0.2f;
+            if (curPullForce < minPullForce) curPullForce = minPullForce;
+            if (CheckSocketStatus(other.gameObject)) ActivateSocket(other.gameObject);
+            ballRb.AddForce((transform.position - other.transform.position) * curPullForce);
+        }
+        //Työntää viimeiseksi osunutta objektia pois jos ForceField päällä
+        else if (isForceFieldActive)
         {
             Vector3 awayDirection = (other.transform.position - transform.position).normalized;
             awayDirection = awayDirection - new Vector3(0, awayDirection.y, 0);
             if(collRb != null) collRb.AddForce(awayDirection * (collRb.velocity.magnitude + 10) * forceFieldPower);
         }
-
-        //Imee palloa sisään päin
-        if (other.tag == "ball")
-        {
-            curPullForce -= 0.2f;
-            if (curPullForce < minPullForce) curPullForce = minPullForce;
-            if (CheckSocketStatus(other.transform)) ActivateSocket();
-            ballRb.AddForce((transform.position - other.transform.position) * curPullForce);
-        }
+        
     }
     
     void TriggerTargetObject()
@@ -61,28 +73,51 @@ public class BallSocket : MonoBehaviour {
         targetObj.GetComponent<BallTriggerObject>().BallTrigger();
     }
 
-    //Katsoo onko objekti paikallaan socketissa ja jos on ollut tarpeeksi kauan paikallaan, palauttaa True.
-    bool CheckSocketStatus(Transform obj)
+    //Katsoo onko oikea objekti paikallaan socketissa ja jos on ollut tarpeeksi kauan paikallaan, palauttaa True.
+    bool CheckSocketStatus(GameObject socketedObj)
     {
+        //if (socketedObj != targetBall) return false;
         if (socketTimer > maxSocketTimer) return true;
 
-        if (Vector3.Distance(obj.position, transform.position) < socketedDistance) socketTimer += Time.deltaTime;
+        if (Vector3.Distance(socketedObj.transform.position, transform.position) < socketedDistance) socketTimer += Time.deltaTime;
         else socketTimer = 0;
 
         return false;
     }
 
-    void ActivateSocket()
+    //Aktivoi ForceFieldin ja jos oikea pallo, näyttää sen efektillä ja triggeröi targetobjektin
+    void ActivateSocket(GameObject socketedObj)
     {
         if (isForceFieldActive) return;
-        ballRb.gameObject.GetComponent<Renderer>().material = activatedMaterial;
         isForceFieldActive = true;
-        TriggerTargetObject();
+
+        if (socketedObj == targetBall)
+        {
+            ballRb.gameObject.GetComponent<Renderer>().material = activatedMaterial;
+            TriggerTargetObject();
+        }
+
+
+        /*if (socketedObj == targetBall)
+        {
+            ballRb.gameObject.GetComponent<Renderer>().material = activatedMaterial;
+            isForceFieldActive = true;
+            TriggerTargetObject();
+        }*/
     }
 
-    void DeactivateSocket()
+    //Nollaa soketin muuttujat ja pelaaja pystyy työntämään pallon pois siitä
+    void ResetSocket()
     {
-        //Tarvitaan tai sitten ei?
+        ball = null; ballRb = null; collRb = null;
+        isForceFieldActive = false;
+        socketTimer = 0;
+    }
+
+    //Testi, Reset erilliseen pelimaailman objektiin(?)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M)) ResetSocket();
     }
 
     //Tekee pienistä vektoreista isoja ja päinvastoin.
