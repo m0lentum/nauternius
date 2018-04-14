c6f9fd3986215594f60e7350f98ac489cab62994
Saska Sinkkonen <saansink@student.jyu.fi>
2018-04-14 16:48:44 +0300

    Vauhtiraunioiden hiontaa

--- Assets/Scripts/CollapsingPlatform.cs
+++ Assets/Scripts/CollapsingPlatform.cs
@@ -6,30 +6,21 @@ using UnityEngine;
 // Copyright © Janne Isoaho, Aarne Manneri, Mikael Myyrä, Lauri Niskanen, Saska Sinkkonen
 //---------------------------------------------------------------------------------------
 
-    //todo timereita pois, muuttujien yms. siistiminen
 public class CollapsingPlatform : MonoBehaviour {
 
     [SerializeField] private float animationWaitTime;
     [SerializeField] private float resetTime;
-
-    private GameObject platform;
-    private Renderer rend;
-    private Collider coll;
+    
+    private AudioSource aSource;
     private Animator anim;
     private float dropTimer;
     private float resetTimer;
     private bool timerStarted;
-    private Vector3 startingPos;
-    private AudioSource aSource;
 
 	private void Awake ()
     {
-        platform = transform.parent.gameObject;
-        startingPos = platform.transform.position;
-        rend = platform.GetComponent<Renderer>();
-        coll = platform.GetComponent<Collider>();
-        anim = platform.GetComponent<Animator>();
-        aSource = platform.GetComponent<AudioSource>();
+        anim = transform.parent.gameObject.GetComponent<Animator>();
+        aSource = transform.parent.gameObject.GetComponent<AudioSource>();
 	}
 	
 	void Update ()
@@ -61,14 +52,12 @@ public class CollapsingPlatform : MonoBehaviour {
 
     void DropPlatform()
     {
-        Debug.Log("dropplat kutsuttu");
         aSource.Play();
         anim.SetBool("DropTriggered", true);
     }
 
     public void ResetPlatform()
     {
-        Debug.Log("reset kutsuttu");
         anim.SetBool("DropTriggered", false);
         timerStarted = false;
         dropTimer = 0f;
