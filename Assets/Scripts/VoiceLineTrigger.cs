c9b93ffa2774b8601dbde9a05a7ae3ad4aa7f5de
Saska Sinkkonen <saansink@student.jyu.fi>
2018-05-04 17:14:02 +0300

    Voiceline testejä sceneen

--- Assets/Scripts/VoiceLineTrigger.cs
+++ Assets/Scripts/VoiceLineTrigger.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//---------------------------------------------------------------------------------------
+// Copyright © Janne Isoaho, Aarne Manneri, Mikael Myyrä, Lauri Niskanen, Saska Sinkkonen
+//---------------------------------------------------------------------------------------
+
 public class VoiceLineTrigger : MonoBehaviour {
     
-    private bool triggered;
+    public bool Triggered { get; set; }
     private AudioSource aSource;
-
-
+    private List<VoiceLineTrigger> sameVoiceLine = new List<VoiceLineTrigger>();
+    
     private void Start()
     {
         aSource = GetComponent<AudioSource>();
+
+        //Lisää listaan kaikki lapsi-skriptit eli skriptit, joissa sama voiceline eri triggerboxissa
+        if (transform.parent != null)
+        {
+            foreach (VoiceLineTrigger vcl in transform.parent.GetComponentsInChildren<VoiceLineTrigger>()) sameVoiceLine.Add(vcl);
+        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !triggered)
+        if (other.CompareTag("Player") && !Triggered)
         {
             aSource.Play();
-            triggered = true;
+            foreach (VoiceLineTrigger vlc in sameVoiceLine) vlc.Triggered = true;
         }
-
-
     }
 }
