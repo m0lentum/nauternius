33adefb0cc5e48477594ca9db2ab089ae6c4b13f
Mikael Myyrä <mikael.myyrae@gmail.com>
2018-05-11 23:11:35 +0300

    Lisätty skrollaavat valoefektit pelaajan moottoreihin

---
+++ Assets/Scripts/UVScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScroller : MonoBehaviour {
+
+    [SerializeField] private Vector2 animationRate = new Vector2(0, 0.07f);
+    private Vector2 totalOffset;
+
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+	void Update ()
+    {
+		if (rend.enabled)
+        {
+            totalOffset += animationRate;
+            rend.materials[0].mainTextureOffset = totalOffset;
+        }
+	}
+}
