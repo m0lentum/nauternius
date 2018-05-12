a496096e97dcbb3ca40f07c424404defa248d84c
Saska Sinkkonen <saansink@student.jyu.fi>
2018-05-12 16:45:39 +0300

    Tosi simppeli menuscene

---
+++ Assets/Scripts/Menu/OpenMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour {
+
+    Canvas exitMenu;
+
+    private void Start()
+    {
+        exitMenu = gameObject.GetComponent<Canvas>();
+    }
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) exitMenu.enabled = !exitMenu.enabled;
+    }
+
+    public void DisableCanvas()
+    {
+        exitMenu.enabled = false;
+    }
+}
