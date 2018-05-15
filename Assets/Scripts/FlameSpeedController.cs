460d6db329b8b33bc727fbf915a2bb8de9e21584
Mikael Myyrä <mikael.myyrae@gmail.com>
2018-05-15 21:21:57 +0300

    Pelaajan moottorit kääntyy ohjauksesta
    Lisäksi valoefektien nopeus muuttuu

---
+++ Assets/Scripts/FlameSpeedController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameSpeedController : MonoBehaviour {
+
+    [SerializeField] private float minSpeed = 0.01f;
+    [SerializeField] private float maxSpeed = 0.04f;
+    [SerializeField] private bool useHorizontalAxis;
+
+    private UVScroller scroller;
+
+    void Start()
+    {
+        scroller = GetComponent<UVScroller>();
+    }
+
+    void Update()
+    {
+        float input = useHorizontalAxis ? Input.GetAxis("Steer") : Input.GetAxis("Forward") - Input.GetAxis("Reverse");
+        scroller.animationRate.y = Mathf.Lerp(minSpeed, maxSpeed, Mathf.Abs(input));
+    }
+}
