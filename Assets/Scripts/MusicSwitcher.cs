1c529b60efac57c68932a455eaf19098e34541f6
Mikael Myyrä <mikael.myyrae@gmail.com>
2018-05-18 10:45:00 +0300

    Lisätty trigger-objekti joka vaihtaa taustamusiikin

---
+++ Assets/Scripts/MusicSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour {
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioClip clip;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && musicSource.clip != clip)
+        {
+            musicSource.clip = clip;
+            musicSource.Play();
+        }
+    }
+}
